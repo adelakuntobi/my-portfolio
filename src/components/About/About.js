8eb81102c57236bd1a5301f10649768d033cf225
adelakuntobi <adelakuntobiloba1@gmail.com>
2020-10-03 11:10:29 +0100

    Added about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,8 +9,10 @@ function About() {
       <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center w-full lg:w-11/12">
         <div className="">
           <Heading name="About me" />
-          <p className="my-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumyeirmod temporinvidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. vero eos et accusamet justo duo dolores et ea rebum. Stet clita kasd gubergren, noseatakimatasanctus est Lorem</p>
-          <p className="my-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumyeirmod temporinvidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. vero eos et accusamet justo</p>
+          <p className="my-2">I am a Adelakun Oluwatobiloba, pretty good wit developing, building, and designing web pages, applications across various platforms. </p>
+
+          <p className="my-2">In my many years of development, I have acquired skill sets in various project</p>
+        
           <h2 className="text-3xl my-5">My Skillsets</h2>
           <div className="flex justify-between skills">
             <ul>
@@ -30,8 +32,12 @@ function About() {
             </ul>
           </div>
           <button className="border-transparent hover:border my-6 py-2 greenBtn w-full md:w-5/12">
+            <a target="_blank" rel="noopener noreferrer"
+             href="https://drive.google.com/file/d/1SnPDJD8JqVsjwt6AZQwTxKXWSZUMfEdV/view?usp=sharing">
           <FontAwesomeIcon icon={['fas', 'download']} className="mx-2" />
-            Download CV</button>
+            Download CV
+            </a>
+            </button>
         </div>
       <img className="w-full md:10/12 lg:w-1/2" src={myImage} alt="Client" />
       </div>
